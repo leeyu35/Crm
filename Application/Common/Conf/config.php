4814aad12bc94bd379db4ded29515864658fb3eb
136187101 <136187101@qq.com>
2016-09-19 14:14:52 +0800

    修改数据库配置文件2

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,7 +8,7 @@ $config_1= array(
 
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
     'DB_NAME'               =>  'crm',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  'hjd136187101',          // 密码
