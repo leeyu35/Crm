1893a5dbd42b5c1309861e8099c66ac7bdc42a3e
136187101 <136187101@qq.com>
2016-09-30 14:02:26 +0800

    更改

--- Application/Admin/Controller/MvController.class.php
+++ Application/Admin/Controller/MvController.class.php
@@ -27,8 +27,27 @@ class MvController extends CommonController
     public function addru(){
         $users1=I('post.users1');//被转移
         $users2=I('post.users2');//接受转移者
+        if(is_array(I('post.kehu')))
+        {
+            foreach (I('post.kehu') as $key=>$val)
+            {
+                $Customer=M("Customer")->where("id=$val")->setField("submituser",$users2);//客户所属权
+                
+                $hetong=M("Contract")->where("advertiser=$val")->setField("submituser",$users2);//合同所属权
+                $Diankuan=M("Diankuan")->where("advertiser=$val")->setField("submituser",$users2);//垫款所属权
+                $Refund=M("Refund")->where("advertiser=$val")->setField("submituser",$users2);//退款所属权
+                $Invoice=M("Invoice")->where("invoice_head=$val")->setField("submituser",$users2);//发票所属权
+                $RefundInvoice=M("RefundInvoice")->where("invoice_head=$val")->setField("submituser",$users2);//退票所属权
+                $this->success("转移成功");
+            }
 
+        }else
+        {
+           $this->error("您好像没有勾选客户哦~");
+        }
 
+        exit;
+        /* 转移全部客户
         $Customer=D("Customer")->where("submituser=$users1")->setField("submituser",$users2);//客户所属权
         $hetong=M("Contract")->where("submituser=$users1")->setField("submituser",$users2);//合同所属权
         $Diankuan=M("Diankuan")->where("submituser=$users1")->setField("submituser",$users2);//垫款所属权
@@ -36,9 +55,19 @@ class MvController extends CommonController
         $Invoice=M("Invoice")->where("submituser=$users1")->setField("submituser",$users2);//发票所属权
         $RefundInvoice=M("RefundInvoice")->where("submituser=$users1")->setField("submituser",$users2);//退票所属权
         $this->success("转移成功");
-
+        */
     }
 
+    public function kehu(){
+        $coustomer=M('Customer');
+        $id=I('post.id');
+        $list=$coustomer->field('id,advertiser')->where("submituser=$id")->order("ctime desc")->select();
+        foreach ($list as $key=>$val)
+        {
+            $str.='<label class="checkbox-inline"><input class="checkbox_bo" name="kehu[]" type="checkbox" id="inlineCheckbox1" value="'.$val['id'].'">'.$val['advertiser'].'</label>    ';
+        }
+        echo $str;
+    }
 
 
 
