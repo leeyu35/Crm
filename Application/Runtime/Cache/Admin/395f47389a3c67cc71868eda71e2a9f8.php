14fa53b6ae5c9e88ae3cc7393d2ba6a88e1e0b2b
136187101 <136187101@qq.com>
2016-10-11 18:05:03 +0800

    客户修改联系人显示
    客户修改联系人显示

--- Application/Runtime/Cache/Admin/395f47389a3c67cc71868eda71e2a9f8.php
+++ Application/Runtime/Cache/Admin/395f47389a3c67cc71868eda71e2a9f8.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -14,13 +14,13 @@
 <script language="javascript">
 $(document).ready(function(e) {
     $("#new_contact").click(function(){
-		$("#contactmain").append('<div class="form-group"><div class="col-sm-1"><input type="text" class="form-control" name="name_n[]" id="inputEmail3" placeholder="联系人"></div><div class="col-sm-2"><input type="text" name="qq_n[] "placeholder="QQ"  class="form-control" id="inputEmail3"></div><div class="col-sm-2"><input type="text" name="weixin_n[]" placeholder="微信" class="form-control" id="inputEmail3"></div><div class="col-sm-2"><input type="text" name="email_n[]" placeholder="邮箱"  class="form-control" id="inputEmail3"></div><div class="col-sm-2"><input type="text" name="position_n[]" placeholder="职位" class="form-control" id="inputEmail3"></div><div class="col-sm-2"><input type="text" name="tel_n[]" placeholder="电话" class="form-control" id="inputEmail3"></div></div>')
+		$("#contactmain").append('<div class="form-group"><div class="col-sm-1"><input type="text" class="form-control" name="name_n[]" id="inputEmail3" placeholder="联系人"></div><div class="col-sm-2"><input type="text" name="qq_n[]"  class="form-control" id="inputEmail3"></div><div class="col-sm-2"><input type="text" name="weixin_n[]"  class="form-control" id="inputEmail3"></div><div class="col-sm-2"><input type="text" name="email_n[]"  class="form-control" id="inputEmail3"></div><div class="col-sm-2"><input type="text" name="position_n[]"  class="form-control" id="inputEmail3"></div><div class="col-sm-2"><input type="text" name="tel_n[]" class="form-control" id="inputEmail3"></div></div>')
 	})
 	$(".de_contact").click(function(){
 		deid=$(this).attr("id");
 		
 		
-		$.get("{:U("delete_contact")}",{id:deid,stime:+Math.random()},function(index){
+		$.get("<?php echo U("delete_contact");?>",{id:deid,stime:+Math.random()},function(index){
 			if(index=='1')
 			{
 				
@@ -43,9 +43,9 @@ $(document).ready(function(e) {
 <div class="container" style="width:100%;" >
 <h3 class="bor-left-bull" >修改客户<small>update Customer</small></h3>
 <br>
-<form action="{:U('upru')}" method="post" class="form-horizontal">
-<input type="hidden" name="id" value="{$info[id]}">
-<input type="hidden" name="time" value="{$info[ctime]}">
+<form action="<?php echo U('upru');?>" method="post" class="form-horizontal">
+<input type="hidden" name="id" value="<?php echo ($info[id]); ?>">
+<input type="hidden" name="time" value="<?php echo ($info[ctime]); ?>">
 <h4>客户基本信息</h4>
 <hr>
 
@@ -53,14 +53,14 @@ $(document).ready(function(e) {
   
     <label for="inputEmail3" class="col-sm-2 control-label">广告主公司名称</label>
     <div class="col-sm-3">
-      <input name="advertiser" type="text" class="form-control" value="{$info['advertiser']}" id="inputEmail3">
+      <input name="advertiser" type="text" class="form-control" value="<?php echo ($info['advertiser']); ?>" id="inputEmail3">
     </div>
     
     <label for="inputEmail3" class="col-sm-1 control-label">客户类型</label>
     <div class="col-sm-3">
       <select name="type"  class="form-control" id="type">
-      	<option value="1" {$info['type']=='1'?'selected':''} >公司</option>
-      	<option value="2" {$info['type']=='2'?'selected':''}>个人</option>
+      	<option value="1" <?php echo ($info['type']=='1'?'selected':''); ?> >公司</option>
+      	<option value="2" <?php echo ($info['type']=='2'?'selected':''); ?>>个人</option>
       </select>
     </div> 
 
@@ -73,14 +73,7 @@ $(document).ready(function(e) {
     <div class="col-sm-10 text-left">
     	
        
-       <volist id="product_line_list" name="product_line_list" key="k">
-       	
-       	
-			<label class="checkbox-inline"  {$k=='1'?'style="margin-left:10px"':''}><input  name="product_line[]" type="checkbox" id="inlineCheckbox1" value="{$product_line_list.id}" <in name="product_line_list.id" value="$info[product_line]">
-        	checked
-        </in>>{$product_line_list.name}</label>
-        
-       </volist>
+       <?php if(is_array($product_line_list)): $k = 0; $__LIST__ = $product_line_list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$product_line_list): $mod = ($k % 2 );++$k;?><label class="checkbox-inline"  <?php echo ($k=='1'?'style="margin-left:10px"':''); ?>><input  name="product_line[]" type="checkbox" id="inlineCheckbox1" value="<?php echo ($product_line_list["id"]); ?>" <?php if(in_array(($product_line_list["id"]), is_array($info[product_line])?$info[product_line]:explode(',',$info[product_line]))): ?>checked<?php endif; ?>><?php echo ($product_line_list["name"]); ?></label><?php endforeach; endif; else: echo "" ;endif; ?>
       
     </div>
     
@@ -94,7 +87,7 @@ $(document).ready(function(e) {
     <div class="col-sm-3">
       <!--<input name="industry" type="text" class="form-control" id="inputEmail3">-->
       <select name="industry"  class="form-control"  id="inputEmail5">
-      <option value="{$info[industry]}" selected>{$info[industry]}</option>
+      <option value="<?php echo ($info[industry]); ?>" selected><?php echo ($info[industry]); ?></option>
       <option value="APP/教育学习">APP/教育学习</option>
       <option value="APP/系统工具">APP/系统工具</option>
       <option value="APP/旅游出行">APP/旅游出行</option>
@@ -158,7 +151,7 @@ $(document).ready(function(e) {
     
     <label for="inputEmail3" class="col-sm-1 control-label">公司官网</label>
     <div class="col-sm-3">      
-		<input name="website" type="text" value="{$info['website']}" class="form-control" id="inputEmail3">
+		<input name="website" type="text" value="<?php echo ($info['website']); ?>" class="form-control" id="inputEmail3">
     </div>
 
   </div>
@@ -168,16 +161,16 @@ $(document).ready(function(e) {
     <label for="inputEmail3" class="col-sm-2 control-label">是否有APP</label>
     <div class="col-sm-3">
     <label class="radio-inline">
-    	<input name="isapp" type="radio" id="inlineRadio1" value="1" {$info['isapp']=='1'?'checked':''} > 有
+    	<input name="isapp" type="radio" id="inlineRadio1" value="1" <?php echo ($info['isapp']=='1'?'checked':''); ?> > 有
     </label>
     <label class="radio-inline">
-    	<input type="radio" name="isapp" id="inlineRadio2" value="0" {$info['isapp']=='0'?'checked':''} > 没有
+    	<input type="radio" name="isapp" id="inlineRadio2" value="0" <?php echo ($info['isapp']=='0'?'checked':''); ?> > 没有
     </label>
     </div>
     
     <label for="inputEmail3" class="col-sm-1 control-label">APP名称</label>
     <div class="col-sm-3">      
-		<input name="appName" type="text" class="form-control" value="{$info['appname']}" id="inputEmail3">
+		<input name="appName" type="text" class="form-control" value="<?php echo ($info['appname']); ?>" id="inputEmail3">
     </div>
 
   </div>
@@ -186,7 +179,7 @@ $(document).ready(function(e) {
     <label for="inputEmail8" class="col-sm-2 control-label">客户所属省</label>
     <div class="col-sm-3">
 	<select name="city"  class="form-control" id="inputEmail4">
-    <option value="{$info[city]}" selected>{$info[city]}</option>
+    <option value="<?php echo ($info[city]); ?>" selected><?php echo ($info[city]); ?></option>
 <option value="北京市">北京市</option> 
 <option value="天津市">天津市</option>
 <option value="上海市">上海市</option>
@@ -229,50 +222,48 @@ $(document).ready(function(e) {
   <h4>联系人<small>&nbsp;&nbsp;<a  id="new_contact" style="cursor:pointer;"><span class="glyphicon glyphicon-plus"></span>新增联系人</a></small></h4>
   <hr>
   <div id="contactmain">
-  <volist id="contact_list" name="contact_list" key="k">
-	<div class="form-group" id="contact_list_id_{$contact_list[id]}">
-    <input type="hidden" name="contactid[]" value="{$contact_list[id]}">
+  <?php if(is_array($contact_list)): $k = 0; $__LIST__ = $contact_list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$contact_list): $mod = ($k % 2 );++$k;?><div class="form-group" id="contact_list_id_<?php echo ($contact_list[id]); ?>">
+    <input type="hidden" name="contactid[]" value="<?php echo ($contact_list[id]); ?>">
     <div class="col-sm-1">
-    	<label {$k!='1'?'style="display:none;"':''}  class="control-label">联系人</label>
-      	<input type="text" class="form-control" name="name[]" value="{$contact_list[name]}" id="inputEmail3" >
+    	<label <?php echo ($k!='1'?'style="display:none;"':''); ?>  class="control-label">联系人</label>
+      	<input type="text" class="form-control" name="name[]" value="<?php echo ($contact_list[name]); ?>" id="inputEmail3" >
     </div>
     
     <div class="col-sm-2">
-    	<label {$k!='1'?'style="display:none;"':''} class="control-label">QQ</label>
-      	<input type="text" class="form-control" name="qq[]" value="{$contact_list[qq]}" id="inputEmail3">
+    	<label <?php echo ($k!='1'?'style="display:none;"':''); ?> class="control-label">QQ</label>
+      	<input type="text" class="form-control" name="qq[]" value="<?php echo ($contact_list[qq]); ?>" id="inputEmail3">
     </div>
     
     
     <div class="col-sm-2">
-    	<label {$k!='1'?'style="display:none;"':''} class="control-label">微信</label>
-      	<input type="text" class="form-control" name="weixin[]"  value="{$contact_list[weixin]}" id="inputEmail3">
+    	<label <?php echo ($k!='1'?'style="display:none;"':''); ?> class="control-label">微信</label>
+      	<input type="text" class="form-control" name="weixin[]"  value="<?php echo ($contact_list[weixin]); ?>" id="inputEmail3">
     </div>
 
     
     <div class="col-sm-2">
-    	<label {$k!='1'?'style="display:none;"':''} class="control-label">邮箱</label>
-      	<input type="text" class="form-control" name="email[]"  value="{$contact_list[email]}" id="inputEmail3">
+    	<label <?php echo ($k!='1'?'style="display:none;"':''); ?> class="control-label">邮箱</label>
+      	<input type="text" class="form-control" name="email[]"  value="<?php echo ($contact_list[email]); ?>" id="inputEmail3">
     </div>
     
    
     <div class="col-sm-2">
-    	<label {$k!='1'?'style="display:none;"':''} class="control-label">职位</label>
-      	<input type="text" class="form-control" name="position[]"  value="{$contact_list[position]}" id="inputEmail3">
+    	<label <?php echo ($k!='1'?'style="display:none;"':''); ?> class="control-label">职位</label>
+      	<input type="text" class="form-control" name="position[]"  value="<?php echo ($contact_list[position]); ?>" id="inputEmail3">
     </div>
     
     
     <div class="col-sm-2">
-    	<label {$k!='1'?'style="display:none;"':''} class="control-label">电话</label>
-      	<input type="text" class="form-control" name="tel[]" value="{$contact_list[tel]}" id="inputEmail3">
+    	<label <?php echo ($k!='1'?'style="display:none;"':''); ?> class="control-label">电话</label>
+      	<input type="text" class="form-control" name="tel[]" value="<?php echo ($contact_list[tel]); ?>" id="inputEmail3">
     </div>
     
     <div class="col-sm-1">
-    	<label {$k!='1'?'style="display:none;"':''} class="control-label" style="color:#fff;">操作执行 </label>
-    	<input type="button" value="删" id="{$contact_list[id]}" class="btn btn-danger de_contact">
+    	<label <?php echo ($k!='1'?'style="display:none;"':''); ?> class="control-label" style="color:#fff;">操作执行 </label>
+    	<input type="button" value="删" id="<?php echo ($contact_list[id]); ?>" class="btn btn-danger de_contact">
     </div>
 
-  </div>
-  </volist>
+  </div><?php endforeach; endif; else: echo "" ;endif; ?>
  </div>
    <h4>开票资料</h4>
   <hr>
@@ -280,12 +271,12 @@ $(document).ready(function(e) {
   
     <label for="inputEmail3" class="col-sm-2 control-label">纳税人识别号</label>
     <div class="col-sm-3">
-      <input name="tax_identification" value="{$info['tax_identification']}" type="text" class="form-control" id="inputEmail3">
+      <input name="tax_identification" value="<?php echo ($info['tax_identification']); ?>" type="text" class="form-control" id="inputEmail3">
     </div>
     
     <label for="inputEmail3" class="col-sm-1 control-label">开票地址</label>
     <div class="col-sm-3">      
-		<input name="ticket_address" value="{$info['ticket_address']}"  type="text" class="form-control" id="inputEmail3">
+		<input name="ticket_address" value="<?php echo ($info['ticket_address']); ?>"  type="text" class="form-control" id="inputEmail3">
     </div>
 
   </div>
@@ -293,12 +284,12 @@ $(document).ready(function(e) {
   
     <label for="inputEmail3" class="col-sm-2 control-label">开户行</label>
     <div class="col-sm-3">
-      <input name="open_account" value="{$info['open_account']}"  type="text" class="form-control" id="inputEmail3">
+      <input name="open_account" value="<?php echo ($info['open_account']); ?>"  type="text" class="form-control" id="inputEmail3">
     </div>
     
     <label for="inputEmail3" class="col-sm-1 control-label">账号</label>
     <div class="col-sm-3">      
-		<input name="account" value="{$info['account']}"  type="text" class="form-control" id="inputEmail3">
+		<input name="account" value="<?php echo ($info['account']); ?>"  type="text" class="form-control" id="inputEmail3">
     </div>
 
   </div>
@@ -306,7 +297,7 @@ $(document).ready(function(e) {
   
     <label for="inputEmail3" class="col-sm-2 control-label">电话</label>
     <div class="col-sm-3">
-      <input name="kp_tel" value="{$info['kp_tel']}"  type="text" class="form-control" id="inputEmail3">
+      <input name="kp_tel" value="<?php echo ($info['kp_tel']); ?>"  type="text" class="form-control" id="inputEmail3">
     </div>
     
 
