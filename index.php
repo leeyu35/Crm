4d0691f19c0c96342a3021cfdf83204d0bbd8b21
136187101 <136187101@qq.com>
2016-10-28 14:26:34 +0800

    安装PostgreSQL扩展完毕

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // 应用入口文件
-die("<h1>Crm Maintenance, please visit later....</h1>");
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
