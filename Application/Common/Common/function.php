3d407f1942697f1da2f63db43fa4dd3c54787c59
136187101 <136187101@qq.com>
2016-11-24 15:31:43 +0800

    curl方法

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -14,3 +14,24 @@ function crm_record($str){
     fclose($txt);
 
 }
+
+//getcul获取数据 并将json 转换成数组 s
+function hjd_curl($url){
+    //初始化
+    $ch=curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+// 3. 执行一个cURL会话并且获取相关回复
+    $response = curl_exec($ch);
+    $response=json_decode($response,true);
+    if ($response  === FALSE) {
+        echo "cURL 具体出错信息: " . curl_error($ch);
+        exit;
+    }
+    $curl_info= curl_getinfo($ch);
+
+// 4. 释放cURL句柄,关闭一个cURL会话
+    curl_close($ch);
+    return $response;
+}
