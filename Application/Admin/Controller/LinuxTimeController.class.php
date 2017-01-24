ded660c7cb81cfd506c1fa5e9a450e2e01b3f032
136187101 <136187101@qq.com>
2017-01-24 18:03:14 +0800

    每天自动获取羽扇数据

--- Application/Admin/Controller/LinuxTimeController.class.php
+++ Application/Admin/Controller/LinuxTimeController.class.php
@@ -84,7 +84,7 @@ class LinuxTimeController extends Controller
         //缓存每个客户具体消费情况 appid ,日期,消费  获取周消费的时候要调用缓存 所以在这里先生存缓存
         $tabledata = M("accountdaily", "baiduapi_", "pgsql://rdspg:anmeng@rds455ekt1422z8sh7e2o.pg.rds.aliyuncs.com:3432/msdb");
 
-        $account_day_cost = $tabledata->field('appid,date,baidu_cost_total,baidu_click_total')->where("date='$date'")->select();
+        $account_day_cost = $tabledata->field('appid,date,baidu_cost_total,baidu_click_total')->where("date='$date' and device='all'")->select();
         if(!$account_day_cost)
         {
             $data['code']=403;
